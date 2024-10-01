903fd7d4d68a89707cb651f6f16e4c6eb101ddca
Flying_Eagle <109035115+FlyingEagl3@users.noreply.github.com>
2024-10-01 10:46:02 +0530

    Create Encoder.go

---
+++ Encoder.go
@@ -0,0 +1,488 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+var (
+	verbose = flag.Bool("v", false, "increase verbosity")
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		log.Fatal("please provide a payload to encode")
+	}
+
+	payload := flag.Arg(0)
+
+	encoders := []encoder{
+		{"URL Encoding", urlEncode},
+		{"HTML Entity Encoding", htmlEntityEncode},
+		{"Unicode Encoding", unicodeEncode},
+		{"ASCII Encoding", asciiEncode},
+		{"UTF-7 Encoding", utf7Encode},
+		{"UTF-8 Encoding", utf8Encode},
+		{"UTF-16 Encoding", utf16Encode},
+		{"UTF-32 Encoding", utf32Encode},
+		{"ISO-8859-1 Encoding", iso88591Encode},
+		{"ISO-8859-2 Encoding", iso88592Encode},
+		{"ISO-8859-5 Encoding", iso88595Encode},
+		{"ISO-8859-6 Encoding", iso88596Encode},
+		{"ISO-8859-7 Encoding", iso88597Encode},
+		{"ISO-8859-8 Encoding", iso88598Encode},
+		{"ISO-8859-9 Encoding", iso88599Encode},
+		{"ISO-8859-10 Encoding", iso885910Encode},
+		{"ISO-8859-11 Encoding", iso885911Encode},
+		{"ISO-8859-13 Encoding", iso885913Encode},
+		{"ISO-8859-14 Encoding", iso885914Encode},
+		{"ISO-8859-15 Encoding", iso885915Encode},
+		{"ISO-8859-16 Encoding", iso885916Encode},
+		{"Windows-1250 Encoding", windows1250Encode},
+		{"Windows-1251 Encoding", windows1251Encode},
+		{"Windows-1252 Encoding", windows1252Encode},
+		{"Windows-1253 Encoding", windows1253Encode},
+		{"Windows-1254 Encoding", windows1254Encode},
+		{"Windows-1255 Encoding", windows1255Encode},
+		{"Windows-1256 Encoding", windows1256Encode},
+		{"Windows-1257 Encoding", windows1257Encode},
+		{"Windows-1258 Encoding", windows1258Encode},
+		{"Binary Encoding", binaryEncode},
+		{"Hex Encoding", hexEncode},
+		{"Octal Encoding", octalEncode},
+		{"Decimal Encoding", decimalEncode},
+		{"JavaScript Encoding", jsEncode},
+		{"JavaScript Unicode Encoding", jsUnicodeEncode},
+		{"JavaScript Octal Encoding", jsOctalEncode},
+		{"Double URL Encoding", doubleUrlEncode},
+		{"Double HTML Entity Encoding", doubleHtmlEntityEncode},
+		{"Double Unicode Encoding", doubleUnicodeEncode},
+		{"Double ASCII Encoding", doubleAsciiEncode},
+		{"Base64 Encoding", base64Encode},
+		{"Base32 Encoding", base32Encode},
+		{"Base16 Encoding", base16Encode},
+		{"Punycode Encoding", punycodeEncode},
+		{"ROT13 Encoding", rot13Encode},
+		{"Caesar Cipher Encoding", caesarCipherEncode},
+		{"Vigenère Cipher Encoding", vigenereCipherEncode},
+		{"XOR Encoding", xorEncode},
+		{"Null Character Encoding", nullCharEncode},
+		{"Tab Character Encoding", tabCharEncode},
+		{"Newline Character Encoding", newlineCharEncode},
+		{"Carriage Return Character Encoding", crCharEncode},
+		{"Space Character Encoding", spaceCharEncode},
+	}
+
+	for _, encoder := range encoders {
+		encoded := encoder.encode(payload)
+		if *verbose {
+			fmt.Printf("%s: %s\n", encoder.name, encoded)
+		} else {
+			fmt.Println(encoded)
+		}
+	}
+}
+
+type encoder struct {
+	name  string
+	encode func(string) string
+}
+
+func urlEncode(s string) string {
+	return strings.Replace(s, " ", "%20", -1)
+}
+
+func htmlEntityEncode(s string) string {
+	return strings.Replace(s, "<", "&lt;", -1)
+}
+
+func unicodeEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("\\u%04x", r)
+	}
+	return encoded
+}
+
+func asciiEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded +=
+func asciiEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("\\%03o", r)
+	}
+	return encoded
+}
+
+func utf7Encode(s string) string {
+	return strings.Replace(s, " ", "+", -1)
+}
+
+func utf8Encode(s string) string {
+	return s
+}
+
+func utf16Encode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("%04x", r)
+	}
+	return encoded
+}
+
+func utf32Encode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("%08x", r)
+	}
+	return encoded
+}
+
+func iso88591Encode(s string) string {
+	return s
+}
+
+func iso88592Encode(s string) string {
+	return s
+}
+
+func iso88595Encode(s string) string {
+	return s
+}
+
+func iso88596Encode(s string) string {
+	return s
+}
+
+func iso88597Encode(s string) string {
+	return s
+}
+
+func iso88598Encode(s string) string {
+	return s
+}
+
+func iso88599Encode(s string) string {
+	return s
+}
+
+func iso885910Encode(s string) string {
+	return s
+}
+
+func iso885911Encode(s string) string {
+	return s
+}
+
+func iso885913Encode(s string) string {
+	return s
+}
+
+func iso885914Encode(s string) string {
+	return s
+}
+
+func iso885915Encode(s string) string {
+	return s
+}
+
+func iso885916Encode(s string) string {
+	return s
+}
+
+func windows1250Encode(s string) string {
+	return s
+}
+
+func windows1251Encode(s string) string {
+	return s
+}
+
+func windows1252Encode(s string) string {
+	return s
+}
+
+func windows1253Encode(s string) string {
+	return s
+}
+
+func windows1254Encode(s string) string {
+	return s
+}
+
+func windows1255Encode(s string) string {
+	return s
+}
+
+func windows1256Encode(s string) string {
+	return s
+}
+
+func windows1257Encode(s string) string {
+	return s
+}
+
+func windows1258Encode(s string) string {
+	return s
+}
+
+func binaryEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("%08b", r)
+	}
+	return encoded
+}
+
+func hexEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("%02x", r)
+	}
+	return encoded
+}
+
+func octalEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("%03o", r)
+	}
+	return encoded
+}
+
+func decimalEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("%d", r)
+	}
+	return encoded
+}
+
+func jsEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("\\x%02x", r)
+	}
+	return encoded
+}
+
+func jsUnicodeEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("\\u%04x", r)
+	}
+	return encoded
+}
+
+func jsOctalEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += fmt.Sprintf("\\%03o", r)
+	}
+	return encoded
+}
+
+func doubleUrlEncode(s string) string {
+	return urlEncode(urlEncode(s))
+}
+
+func doubleHtmlEntityEncode(s string) string {
+	return htmlEntityEncode(htmlEntityEncode(s))
+}
+
+func doubleUnicodeEncode(s string) string {
+	return unicodeEncode(unicodeEncode(s))
+}
+
+func doubleAsciiEncode(s string) string {
+	return asciiEncode(asciiEncode(s))
+}
+
+func base64Encode(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
+
+func base32Encode(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
+
+func base16Encode(s string) string {
+	return hex.EncodeToString([]byte(s))
+}
+
+func punycodeEncode(s string) string {
+	// Punycode encoding is not implemented in Go's standard library
+	// You can use a third-party library like github.com/WeAreFarm/punycode
+	return ""
+}
+
+func rot13Encode(s string) string {
+	var encoded string
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' {
+			encoded += string(rune(((r-'a'+13)%26) + 'a'))
+		} else if r >= 'A' && r <= 'Z' {
+			encoded += string(rune(((r-'A'+13)%26) + 'A'))
+		} else {
+			encoded += string(r)
+		}
+	
+func rot13Encode(s string) string {
+	var encoded string
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' {
+			encoded += string(rune(((r-'a'+13)%26) + 'a'))
+		} else if r >= 'A' && r <= 'Z' {
+			encoded += string(rune(((r-'A'+13)%26) + 'A'))
+		} else {
+			encoded += string(r)
+		}
+	}
+	return encoded
+}
+
+func caesarCipherEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' {
+			encoded += string(rune(((r-'a'+3)%26) + 'a'))
+		} else if r >= 'A' && r <= 'Z' {
+			encoded += string(rune(((r-'A'+3)%26) + 'A'))
+		} else {
+			encoded += string(r)
+		}
+	}
+	return encoded
+}
+
+func vigenereCipherEncode(s string) string {
+	// Vigenère cipher encoding is not implemented in Go's standard library
+	// You can use a third-party library like github.com/WeAreFarm/vigenere
+	return ""
+}
+
+func xorEncode(s string) string {
+	var encoded string
+	for _, r := range s {
+		encoded += string(r ^ 0x01)
+	}
+	return encoded
+}
+
+func nullCharEncode(s string) string {
+	return strings.Replace(s, " ", "\x00", -1)
+}
+
+func tabCharEncode(s string) string {
+	return strings.Replace(s, " ", "\x09", -1)
+}
+
+func newlineCharEncode(s string) string {
+	return strings.Replace(s, " ", "\x0a", -1)
+}
+
+func crCharEncode(s string) string {
+	return strings.Replace(s, " ", "\x0d", -1)
+}
+
+func spaceCharEncode(s string) string {
+	return strings.Replace(s, " ", "\x20", -1)
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		log.Fatal("please provide a payload to encode")
+	}
+
+	payload := flag.Arg(0)
+
+	encoders := []encoder{
+		{"URL Encoding", urlEncode},
+		{"HTML Entity Encoding", htmlEntityEncode},
+		{"Unicode Encoding", unicodeEncode},
+		{"ASCII Encoding", asciiEncode},
+		{"UTF-7 Encoding", utf7Encode},
+		{"UTF-8 Encoding", utf8Encode},
+		{"UTF-16 Encoding", utf16Encode},
+		{"UTF-32 Encoding", utf32Encode},
+		{"ISO-8859-1 Encoding", iso88591Encode},
+		{"ISO-8859-2 Encoding", iso88592Encode},
+		{"ISO-8859-5 Encoding", iso88595Encode},
+		{"ISO-8859-6 Encoding", iso88596Encode},
+		{"ISO-8859-7 Encoding", iso88597Encode},
+		{"ISO-8859-8 Encoding", iso88598Encode},
+		{"ISO-8859-9 Encoding", iso88599Encode},
+		{"ISO-8859-10 Encoding", iso885910Encode},
+		{"ISO-8859-11 Encoding", iso885911Encode},
+		{"ISO-8859-13 Encoding", iso885913Encode},
+		{"ISO-8859-14 Encoding", iso885914Encode},
+		{"ISO-8859-15 Encoding", iso885915Encode},
+		{"ISO-8859-16 Encoding", iso885916Encode},
+		{"Windows-1250 Encoding", windows1250Encode},
+		{"Windows-1251 Encoding", windows1251Encode},
+		{"Windows-1252 Encoding", windows1252Encode},
+		{"Windows-1253 Encoding", windows1253Encode},
+		{"Windows-1254 Encoding", windows1254Encode},
+		{"Windows-1255 Encoding", windows1255Encode},
+		{"Windows-1256 Encoding", windows1256Encode},
+		{"Windows-1257 Encoding", windows1257Encode},
+		{"Windows-1258 Encoding", windows1258Encode},
+		{"Binary Encoding", binaryEncode},
+		{"Hex Encoding", hexEncode},
+		{"Octal Encoding", octalEncode},
+		{"Decimal Encoding", decimalEncode},
+		{"JavaScript Encoding", jsEncode},
+		{"JavaScript Unicode Encoding", jsUnicodeEncode},
+		{"JavaScript Octal Encoding", jsOctalEncode},
+		{"Double URL Encoding", doubleUrlEncode},
+		{"Double HTML Entity Encoding", doubleHtmlEntityEncode},
+		{"Double Unicode Encoding", doubleUnicodeEncode},
+		{"Double ASCII Encoding", doubleAsciiEncode
+		{"Double URL Encoding", doubleUrlEncode},
+		{"Double HTML Entity Encoding", doubleHtmlEntityEncode},
+		{"Double Unicode Encoding", doubleUnicodeEncode},
+		{"Double ASCII Encoding", doubleAsciiEncode},
+		{"Base64 Encoding", base64Encode},
+		{"Base32 Encoding", base32Encode},
+		{"Base16 Encoding", base16Encode},
+		{"Punycode Encoding", punycodeEncode},
+		{"ROT13 Encoding", rot13Encode},
+		{"Caesar Cipher Encoding", caesarCipherEncode},
+		{"Vigenère Cipher Encoding", vigenereCipherEncode},
+		{"XOR Encoding", xorEncode},
+		{"Null Character Encoding", nullCharEncode},
+		{"Tab Character Encoding", tabCharEncode},
+		{"Newline Character Encoding", newlineCharEncode},
+		{"Carriage Return Character Encoding", crCharEncode},
+		{"Space Character Encoding", spaceCharEncode},
+	}
+
+	fmt.Println("Encoder by Flying_Eagle")
+	fmt.Println("Tool Name: Encoder")
+	fmt.Println("")
+
+	for _, encoder := range encoders {
+		encoded := encoder.encode(payload)
+		if *verbose {
+			fmt.Printf("%s: %s\n", encoder.name, encoded)
+		} else {
+			fmt.Println(encoded)
+		}
+	}
+}
